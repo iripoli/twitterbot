72042ee78595f5b15da12b44935d8d13ed08bb35
iripoli <50406400+iripoli@users.noreply.github.com>
2020-01-17 19:55:43 +0100

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    consumer_key: 'j0Xa34sAYIkHGYGviRuEN8eA6',
-    consumer_secret: 'OE7Qg3IORrylymm7QftfFAcEOj913lBuxwHYK5b8y45V5C5wKQ',
-    access_token: '1218109760140128263-onHEiQAhCEIwH0tLuexBks6zpzd8v5',
-    access_token_secret: '8ncvHO2mF87a5ixtei3b4N19M0hL3KRYPAR6txiSqZQeT'
-    }
-    
-    
-
